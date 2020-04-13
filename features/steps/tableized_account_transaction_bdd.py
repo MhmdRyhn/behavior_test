b1326979bbda6ab7ef9eb19d5a301bc830267029
MhmdRyhn <rayhan.cse12@gmail.com>
2020-04-13 20:39:49 +0600

    Implement tablized feature for checking exception

--- features/steps/tableized_account_transaction_bdd.py
+++ features/steps/tableized_account_transaction_bdd.py
@@ -1,6 +1,6 @@
 import behave
 
-from app.account import Account
+from app.account import Account, InsufficientBalance
 
 transaction_mapper = {
     'add': 'add_cash',
@@ -8,16 +8,30 @@ transaction_mapper = {
 }
 
 
-@behave.given('I have an initial amount of {initial:d} in my account')
+@behave.given('I have an initial amount of ${initial:d} in my account')
 def setup_initial_amount(context, initial):
     context.account = Account(initial_amount=initial)
 
 
-@behave.when('I {transaction} {amount:d} to/from my account')
+@behave.when('I {transaction} ${amount:d} to/from my account')
 def make_transaction(context, transaction, amount):
     getattr(context.account, transaction_mapper[transaction])(amount=amount)
 
 
-@behave.then('It becomes {total:d} in my account')
+@behave.then('It becomes ${total:d} in my account')
 def check_total(context, total):
     assert context.account.current_cash == total
+
+
+@behave.when('I withdraw ${withdraw_amount:d} from my account')
+def make_invalid_transaction(context, withdraw_amount):
+    try:
+        context.account.withdraw_cash(amount=withdraw_amount)
+    except InsufficientBalance as ex:
+        context.exc = ex
+
+
+@behave.then('It raises {exception} exception when withdrawing ${withdraw_amount:d} from ${initial:d}')
+def check_exception(context, exception, withdraw_amount, initial):
+    assert isinstance(context.exc, eval(exception))
+    assert str(context.exc) == f'Not enough available balance to withdraw {withdraw_amount}'
