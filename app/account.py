666770154a9d1ae76032a13bad57b5edc0613c26
MhmdRyhn <rayhan.cse12@gmail.com>
2020-04-13 16:22:51 +0600

    add basic app to work with behave

---
+++ app/account.py
@@ -0,0 +1,19 @@
+class InsufficientBalance(Exception):
+    pass
+
+
+class Account:
+    def __init__(self, initial_amount=0):
+        self.balance = initial_amount
+
+    def withdraw_cash(self, amount):
+        if self.balance < amount:
+            raise InsufficientBalance('Not enough available balance to withdraw {}'.format(amount))
+        self.balance -= amount
+
+    def add_cash(self, amount):
+        self.balance += amount
+
+    @property
+    def current_cash(self):
+        return self.balance
